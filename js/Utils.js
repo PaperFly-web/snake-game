338e51a77b067dd55f58f7f291d7a81994a4b550
PaperFly-web <62205559+PaperFly-web@users.noreply.github.com>
2021-05-12 22:05:45 +0800

    Update Utils.js

--- js/Utils.js
+++ js/Utils.js
@@ -3,15 +3,15 @@ const AUDIO_OVER = new Audio('audio/over.mp3');
 let AUDIO_VOLUME = 1;
 
 function getWidthAndHeightAndSize() {
-    height = document.body.clientHeight
-    height = Math.floor(height * 4 / 7);
-    
+    height = document.body.clientHeight;
+    width = document.body.clientWidth;
+
     wh = Math.min(height, width); //去宽和高中的最小值
 
     wh = Math.floor(wh * 4 / 7);
     size = Math.floor(wh / 40);
     wh = wh - wh % size; //高和宽变成size的整数倍
-   return { size, "height": wh, "width": wh }
+    return { size, "height": wh, "width": wh }
 }
 //生成画布中的随机位置
 function randomNum(canvasOptions, size) {
